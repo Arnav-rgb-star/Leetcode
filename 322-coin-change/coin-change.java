cb028b1ca94aa74157f431a7c4ac256879206cd4
Arnav-rgb-star <arnavdubey675@gmail.com>
2026-07-01 09:50:47 +0530

    Time: 17 ms (47.80%) | Memory: 47.1 MB (23.74%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,23 @@
 class Solution {
     int dp[][];
-
-    int f(int i,int a,int c[]){
-        if(i==0){
-            if(a%c[0]==0) return a/c[0];
-            else return (int)1e9;
-        }
-        if(dp[i][a]!=-1) return dp[i][a];
-        int notTake=0+f(i-1,a,c);
-        int take=(int)1e9;
-        if(a>=c[i]) take = 1+f(i,a-c[i],c);
-
-        return dp[i][a]=Math.min(take,notTake);
-    }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         dp=new int[n][amount+1];
 
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        int cnt = f(n-1,amount,coins);
-        if(cnt ==(int)1e9) return -1;
-        else return cnt;
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0) dp[0][i]=i/coins[0];
+            else dp[0][i]=(int)1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int t=0;t<=amount;t++){
+                int take=(int)1e9;
+                int notTake=dp[i-1][t];
+                if(t>=coins[i]) take=1+dp[i][t-coins[i]];
+
+                dp[i][t]=Math.min(take,notTake);
+            }
+        }
+        if(dp[n-1][amount]>=(int)1e9) return -1;
+        else return dp[n-1][amount];
     }
 }
