4647424b0f84d433b1532dc1855737f24a66c0bb
Arnav Dubey <arnavdubey675@gmail.com>
2026-07-13 19:30:03 +0530

    Time: 5 ms (68.85%) | Memory: 47.3 MB (34.43%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,22 +1,25 @@
 class Solution {
     int dp[][];
-    int f(int i,int j,String a,String b){
-        if(i<0) return (j+1);
-        if(j<0) return (i+1);
+    int f(int n,int m,String a,String b){
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(a.charAt(i)==b.charAt(j)) return dp[i][j] = f(i-1,j-1,a,b);
-        else{
-            return dp[i][j] = 1 + Math.min(f(i-1,j,a,b),Math.min(f(i,j-1,a,b),f(i-1,j-1,a,b)));
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int j=0;j<=m;j++) dp[0][j]=j;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a.charAt(i-1)==b.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
         }
-
+        return dp[n][m];
     }
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m=word2.length();
-        dp=new int[n][m];
+        dp=new int[n+1][m+1];
 
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        return f(n-1,m-1,word1,word2);
+        return f(n,m,word1,word2);
     }
 }
