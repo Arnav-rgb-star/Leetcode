8842e31a8e40c41805d17bd8eac951aa8f832fbd
Arnav Dubey <arnavdubey675@gmail.com>
2026-08-13 10:18:19 +0530

    Time: 12 ms (25.91%) | Memory: 86.4 MB (27.21%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,40 +1,29 @@
 class Solution {
-    int l=0;
     int diff=0;
     int dp[][];
-    public int f(int a[], int l,int i){
-        
+    public int f(int a[]){
         int n = a.length;
-        if(i==a.length)  return 0;
-        if(dp[l][i]!=-1) return dp[l][i];
-
-            int one = Integer.MIN_VALUE;
-            int two= Integer.MIN_VALUE;
-            int three=Integer.MIN_VALUE;
-                 
-        if(l==0){
-             one = a[i] - f(a,1,i+1);
-             if(i+1 < n)  two= a[i] + a[i+1] - f(a,1,i+2);
-             if(i+2 < n)  three= a[i] + a[i+1] +a[i+2] - f(a,1,i+3);
-             
-             return dp[l][i] =Math.max(Math.max(one,two),three);
-             }
-             
-              one = a[i] - f(a,0,i+1);
-             if(i+1 < n)  two= a[i] + a[i+1] -f(a,0,i+2);
-             if(i+2 < n)  three= a[i] + a[i+1] +a[i+2] - f(a,0,i+3);
-             
-             return  dp[l][i] = Math.max(Math.max(one,two),three);                  
         
+        for(int i=n-1;i>=0;i--){
+            for(int l=0;l<2;l++){
+                int one = Integer.MIN_VALUE;
+                int two= Integer.MIN_VALUE;
+                int three=Integer.MIN_VALUE;
+
+                    one = a[i] - dp[1-l][i+1];
+                    if(i+1 < n)  two= a[i] + a[i+1] - dp[1-l][i+2];
+                    if(i+2 < n)  three= a[i] + a[i+1] +a[i+2] - dp[1-l][i+3];
+                    
+                    dp[l][i] =Math.max(Math.max(one,two),three);
+            }    
+        }             
+        return dp[0][0];
     }
     public String stoneGameIII(int[] stoneValue) {
         
-        dp = new int[2][stoneValue.length];
+        dp = new int[2][stoneValue.length+1];
 
-        Arrays.fill(dp[0],-1);
-        Arrays.fill(dp[1],-1);
-
-        diff = f(stoneValue,l,0);
+        diff = f(stoneValue);
 
         if(diff==0) return "Tie";
         else if(diff>0) return "Alice";
