6226d8593bfec64a9878f63b349a2fe7cc3932cf
Arnav Dubey <arnavdubey675@gmail.com>
2026-08-19 19:16:38 +0530

    Time: 3 ms (100.00%) | Memory: 114.3 MB (98.09%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int cnt[]= new int [3];
+
+        for(int i=0;i<stones.length;i++) cnt[(stones[i]%3)]++;
+        
+        if((cnt[0]&1) ==0) return (cnt[1]>0 && cnt[2]>0);
+        else return Math.abs(cnt[1]-cnt[2])>=3;
+    }
+}
